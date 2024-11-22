e735419c66ab8b4f83bd081071eb21413552da42
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-11-22 11:29:12 +0100

    time to do your own function

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+const newsBox0 = document.getElementById("box0");
+const newsBox1 = document.getElementById("box1");
+const newsBox2 = document.getElementById("box2");
+const newsBox3 = document.getElementById("box3");
+const newsBox4 = document.getElementById("box4");
+const newsBox5 = document.getElementById("box5");
+const newsBox6 = document.getElementById("box6");
+const newsBox7 = document.getElementById("box7");
+const newsBox8 = document.getElementById("box8");
+const newsBox9 = document.getElementById("box9");
 fetch("https://content.guardianapis.com/search?api-key=022e2d96-2cf9-421e-9b66-bc1ac2780e08")
 .then((response)=>{
 
@@ -12,7 +21,9 @@ fetch("https://content.guardianapis.com/search?api-key=022e2d96-2cf9-421e-9b66-b
     }
 })
 .then((data) => {
-    console.log("data", data.response.results);
+    console.log("data", data.response);
+    sportFunction(data.response.results)
+
 })
 .catch((error) => {
     console.error("u got ERROR sir", error);
@@ -20,11 +31,23 @@ fetch("https://content.guardianapis.com/search?api-key=022e2d96-2cf9-421e-9b66-b
 
 
 
-function createTitle() {
-    for (let i = 0; i < data.length; i++) {
+function sportFunction(arr) {
+    for (let i = 1; i < 2; i++) {
         
         
+        const newsTitle = document.createElement("li");
+        newsTitle.innerText = arr[i].sectionId;    ;
+        const newsDescription = document.createElement("p");
+        newsDescription.innerText = arr[i].webTitle;    ;
+        newsBox0.appendChild(newsTitle);
+        newsBox0.appendChild(newsDescription);
     }
+
+        
+        
+    
+        
+
 }
 function creationDate() {
     
