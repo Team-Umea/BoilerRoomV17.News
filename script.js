09dcc3b2c8f2ce33ed07f144c7a328beeea3c9f8
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-11-22 12:07:25 +0100

    newBZZ functions done

--- script.js
+++ script.js
@@ -22,7 +22,14 @@ fetch("https://content.guardianapis.com/search?api-key=022e2d96-2cf9-421e-9b66-b
 })
 .then((data) => {
     console.log("data", data.response);
-    sportFunction(data.response.results)
+    functionIndex0(data.response.results)
+    functionIndex1(data.response.results)
+    functionIndex2(data.response.results)
+    functionIndex3(data.response.results)
+    functionIndex4(data.response.results)
+    functionIndex5(data.response.results)
+    functionIndex6(data.response.results)
+    functionIndex7(data.response.results)
 
 })
 .catch((error) => {
@@ -31,37 +38,178 @@ fetch("https://content.guardianapis.com/search?api-key=022e2d96-2cf9-421e-9b66-b
 
 
 
-function sportFunction(arr) {
+function functionIndex0(arr) {
+    for (let i = 0; i < 1; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox0.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox0.appendChild(newsDescription);
+        newsBox0.appendChild(newsTime)
+        
+    }  
+}
+function functionIndex1(arr) {
     for (let i = 1; i < 2; i++) {
         
         
         const newsTitle = document.createElement("li");
-        newsTitle.innerText = arr[i].sectionId;    ;
+        const newsTagg = document.createElement("a");
         const newsDescription = document.createElement("p");
-        newsDescription.innerText = arr[i].webTitle;    ;
-        newsBox0.appendChild(newsTitle);
-        newsBox0.appendChild(newsDescription);
-    }
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
 
+        newsBox1.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox1.appendChild(newsDescription);
+        newsBox1.appendChild(newsTime)
+        
+    }  
+
+}
+function functionIndex2(arr) {
+    for (let i = 2; i < 3; i++) {
         
         
-    
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox2.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox2.appendChild(newsDescription);
+        newsBox2.appendChild(newsTime)
         
+    }  
 
 }
-function creationDate() {
-    
-}
-function createPTaggs() {
-    
-}
-function createDescription() {
-    
-}
-function createLiElement() {
-    
-}
-function render() {
-    
-}
+function functionIndex3(arr) {
+    for (let i = 3; i < 4; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
 
+        newsBox3.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox3.appendChild(newsDescription);
+        newsBox3.appendChild(newsTime)
+        
+    }  
+
+}
+function functionIndex4(arr) {
+    for (let i = 4; i < 5; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox4.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox4.appendChild(newsDescription);
+        newsBox4.appendChild(newsTime)
+        
+    }  
+
+}
+function functionIndex5(arr) {
+    for (let i = 5; i < 6; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox5.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox5.appendChild(newsDescription);
+        newsBox5.appendChild(newsTime)
+        
+    }  
+
+}
+function functionIndex6(arr) {
+    for (let i = 6; i < 7; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox6.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox6.appendChild(newsDescription);
+        newsBox6.appendChild(newsTime)
+        
+    }  
+
+}
+function functionIndex7(arr) {
+    for (let i = 7; i < 8; i++) {
+        
+        
+        const newsTitle = document.createElement("li");
+        const newsTagg = document.createElement("a");
+        const newsDescription = document.createElement("p");
+        const newsTime = document.createElement("p")
+        newsTagg.href = arr[i].webUrl;
+        newsTagg.target = "_blank"; 
+        newsTagg.innerText = arr[i].sectionId;
+        newsDescription.innerText = arr[i].webTitle;
+        newsTime.innerText = arr[i].webPublicationDate;
+
+        newsBox7.appendChild(newsTitle);
+        newsTitle.appendChild(newsTagg)
+        newsBox7.appendChild(newsDescription);
+        newsBox7.appendChild(newsTime)
+        
+    }  
+
+}
