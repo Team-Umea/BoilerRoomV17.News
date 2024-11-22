43009c20010255139285b07e6bd616d366d46991
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2024-11-22 09:54:21 +0100

    first fetch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+fetch("https://newsapi.org/v2/everything?q=Apple&from=2024-11-22&sortBy=popularity&apiKey=40cf19549c954aabbf7a9e4c3dd08677")
+.then((response)=>{
+
+    if (!response.ok) {
+        throw new Error("ERROR!!");
+        
+    } else {
+        console.log("response: ",response);
+        
+        response.json()
+    }
+})
+.then((data) => {
+    console.log("data", data);
+})
+.catch((error) => {
+    console.error("u got ERROR sir", error);
+});
